0ee017b1348b76d4bade838c09cd0040c3ed6583
Simon <zachau@in.tum.de>
2018-09-03 18:29:15 +0200

    check login against swagger API

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 ﻿'use strict';
-const debug = require('debug');
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
@@ -17,7 +16,14 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-	res.send('<ul><li><a href="/service-provider-1">service-provider-1</a></li></ul>');
+	res.send(`
+	<h1>Nutzerkonto Bund</h1>
+	<h2>Stammdaten</h2>
+	<p>TODO</p>
+	<h2>Services</h2>
+	<ul>
+		<li><a href="/service-provider-1">service-provider-1</a></li>
+	</ul>`);
 });
 
 app.get('/service-provider-1', (req, res) => {
@@ -32,23 +38,33 @@ app.get('/service-provider-1', (req, res) => {
 });
 
 app.post('/service-provider-1/auth', (req, res) => {
-	const dbResult = usersDB.find((user) => {
-		return (user.username === req.body.username && user.password === req.body.password);
-	})
+	process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-	if (dbResult !== undefined) {
+	axios.get('https://nutzerkonto-db.herokuapp.com/login', {
+		params: {
+			username: req.body.username,
+			password: req.body.password
+		}
+	}).then(response => {
+		console.log(response);
 		let html = `
-		<p>Hi ${req.body.username}! Please enter your 2FA Code:</p>
-		<form action="/service-provider-1/welcome" method="post">
-			<input type="text" name="code" placeholder="Code">
-			<input style="display:none;" type="text" name="username" value="${req.body.username}">
-			<input type="submit" value="submit">
-		</form>`;
+			<p>Hi ${req.body.username}! Please enter your 2FA Code:</p>
+			<form action="/service-provider-1/welcome" method="post">
+				<input type="text" name="code" placeholder="Code">
+				<input style="display:none;" type="text" name="username" value="${req.body.username}">
+				<input type="submit" value="submit">
+			</form>`;
 
 		res.send(html);
-	} else {
-		res.send('Wrong username/password');
-	}
+	}).catch(error => {
+		console.log(error);
+
+		if (error.status != 200) {
+			res.send('Wrong username/password');
+		} else {
+			res.send('2FA service may be down!');
+		}
+	});
 });
 
 app.post('/service-provider-1/welcome', (req, res) => {
@@ -59,16 +75,14 @@ app.post('/service-provider-1/welcome', (req, res) => {
 			user: req.body.username,
 			pass: req.body.code
 		}
-	})
-	.then(response => {
+	}).then(response => {
 		console.log(response);
 		if (response.data.result.value) {
 			res.send('Login successful!');
 		} else {
 			res.send('Wrong 2FA code!');
 		}
-	})
-	.catch(error => {
+	}).catch(error => {
 		console.log(error);
 		res.send('2FA service may be down!');
 	});
