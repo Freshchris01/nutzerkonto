efa07aeeb9dee8ff6cd55bb3431dd61e3afcb06d
Simon <zachau@in.tum.de>
2018-09-17 22:37:21 +0200

    styles

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 module.exports = {
 	DATABASE: 'https://nutzerkonto-db.herokuapp.com',
 	LINOTP: 'https://13.80.108.55',
-	KEYCLOAK: 'https://13.80.41.117:8443/auth'
+	KEYCLOAK: 'https://nutzerkonto.westeurope.cloudapp.azure.com:8443/auth/'
 }
