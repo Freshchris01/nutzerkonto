61637754922f19cad6273f8ebaf8c01e6106f0e8
Simon <zachau@in.tum.de>
2018-10-16 15:42:56 +0200

    config to include only needed params

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 'use strict';
 
 module.exports = {
-	DATABASE: 'https://nutzerkonto-db.herokuapp.com',
-	LINOTP: 'https://13.80.108.55',
 	KEYCLOAK: 'https://nutzerkonto.westeurope.cloudapp.azure.com:8443/auth'
 }
